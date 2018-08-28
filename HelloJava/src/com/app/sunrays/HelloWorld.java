b4852d28c3d6baba44dcd3a63b78c43723f1e5d6
suraj <suraj.sunrays86@outlook.com>
2018-08-28 20:42:25 +0530

    output print

--- HelloJava/src/com/app/sunrays/HelloWorld.java
+++ HelloJava/src/com/app/sunrays/HelloWorld.java
@@ -2,7 +2,7 @@ package com.app.sunrays;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to VCS");
 
 	}
 
