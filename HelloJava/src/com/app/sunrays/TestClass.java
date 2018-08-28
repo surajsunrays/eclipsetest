75f15f56333254b78803b143f0a0879a549985d4
suraj <suraj.sunrays86@outlook.com>
2018-08-28 20:48:19 +0530

    TestClass Created

---
+++ HelloJava/src/com/app/sunrays/TestClass.java
@@ -0,0 +1,10 @@
+package com.app.sunrays;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Im Working Fine....");
+	}
+
+}
